095c8b5d0c0c931b31b16cf30941046d9556b51e
Dekan <dekanbrown@odyssy.io>
2024-07-10 12:09:06 -0600

    update proxy factory address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -53,13 +53,13 @@ export const CURATOR_CONTRACTS: KeychainList = {
     // "0x1": "0xa6b71e26c5e0845f74c812102ca7114b6a896ab2",
     // "0x5": "0xa6b71e26c5e0845f74c812102ca7114b6a896ab2",
     // "0xa": "0xc22834581ebc8527d974f8a1c97e1bea4ef910bc",
-    "0xa4b1": "0xc22834581ebc8527d974f8a1c97e1bea4ef910bc",
+    "0xa4b1": "0xa6b71e26c5e0845f74c812102ca7114b6a896ab2" // "0xc22834581ebc8527d974f8a1c97e1bea4ef910bc", // module factory 0x00000000000DC7F163742Eb4aBEf650037b1f588
   },
   GNOSIS_SAFE_MASTER_COPY: {
     // "0x1": "0xd9db270c1b5e3bd161e8c8503c55ceabee709552",
     // "0x5": "0xd9db270c1b5e3bd161e8c8503c55ceabee709552",
     // "0xa": "0xfb1bffc9d739b8d520daf37df666da4c687191ea", // "0xd9db270c1b5e3bd161e8c8503c55ceabee709552",
-    "0xa4b1": "0xd9db270c1b5e3bd161e8c8503c55ceabee709552",
+    "0xa4b1": "0x3E5c63644E683549055b9Be8653de26E0B4CD36E", // 0x3E5c63644E683549055b9Be8653de26E0B4CD36E
   },
   UNISWAP_V3_NF_POSITION_MANAGER: {
     "0xa4b1": "",
@@ -128,7 +128,7 @@ export const LOOT_NAME = "Community Power";
 export const LOOT_SYMBOL = "LOOT";
 export const DEFAULT_SUMMON_VALUES = {
   //votingPeriodInSeconds: 259200,
-  votingPeriodInSeconds: 201,
+  votingPeriodInSeconds: 200,
   // gracePeriodInSeconds: 172800,
   gracePeriodInSeconds: 6,
   newOffering: "10000000000000000",
